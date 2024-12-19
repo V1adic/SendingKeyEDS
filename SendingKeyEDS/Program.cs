204d614226046dee9a45677a2e5d10e77decf9c2
V1adic <122886131+V1adic@users.noreply.github.com>
2024-12-20 00:39:23 +0500

    Initial commit

---
+++ SendingKeyEDS/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Security.Cryptography;
+using SendingKeyEDS;
+
+internal class Program
+{
+    private static void Main()
+    {
+        GenerateKey.Worker();
+    }
+}
